6c5897eb665d89f41f6e86588216db25a49b563a
Andrew Fitzgerald <apfitzge@gmail.com>
2022-05-18 13:45:08 -0500

    clippy fixes

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -2746,7 +2746,7 @@ impl Blockstore {
                         entry
                             .transactions
                             .iter()
-                            .flat_map(|tx| tx.message.static_account_keys().into_iter().cloned())
+                            .flat_map(|tx| tx.message.static_account_keys().iter().cloned())
                             .collect()
                     })
                     .collect()
