afe98bcba0bdb2d051f961d8064ef31ddf880fbd
Andrew Fitzgerald <apfitzge@gmail.com>
2022-07-12 15:06:03 -0500

    Add command line argument parsing

--- transaction-scheduler-bench/src/main.rs
+++ transaction-scheduler-bench/src/main.rs
@@ -1,3 +1,62 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// How many packets per second to send to the scheduler
+    #[clap(long, env, default_value_t = 200_000)]
+    packet_send_rate: usize,
+
+    /// Number of packets per batch
+    #[clap(long, env, default_value_t = 128)]
+    packets_per_batch: usize,
+
+    /// Number of batches per message
+    #[clap(long, env, default_value_t = 4)]
+    batches_per_msg: usize,
+
+    /// Number of consuming threads (number of threads requesting batches from scheduler)
+    #[clap(long, env, default_value_t = 20)]
+    num_execution_threads: usize,
+
+    /// How long each transaction takes to execution in microseconds
+    #[clap(long, env, default_value_t = 15)]
+    execution_per_tx_us: u64,
+
+    /// Duration of benchmark
+    #[clap(long, env, default_value_t = 20.0)]
+    duration: f32,
+
+    /// Number of accounts to choose from when signing transactions
+    #[clap(long, env, default_value_t = 100000)]
+    num_accounts: usize,
+
+    /// Number of read locks per tx
+    #[clap(long, env, default_value_t = 4)]
+    num_read_locks_per_tx: usize,
+
+    /// Number of write locks per tx
+    #[clap(long, env, default_value_t = 2)]
+    num_read_write_locks_per_tx: usize,
+
+    /// If true, enables state auction on accounts
+    #[clap(long, env)]
+    enable_state_auction: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    solana_logger::setup_with_default("INFO");
+
+    let Args {
+        packet_send_rate,
+        packets_per_batch,
+        batches_per_msg,
+        num_execution_threads,
+        execution_per_tx_us,
+        duration,
+        num_accounts,
+        num_read_locks_per_tx,
+        num_read_write_locks_per_tx,
+        enable_state_auction,
+    } = Args::parse();
 }
