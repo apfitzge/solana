b94bcbb719948d19b27594ab660bba4ef22a8b40
Andrew Fitzgerald <apfitzge@gmail.com>
2022-07-28 10:34:14 -0500

    ReportMetricsInterval trait and derive

--- metrics_reporter/src/lib.rs
+++ metrics_reporter/src/lib.rs
@@ -1,6 +1,14 @@
-pub use {solana_metrics::datapoint_info, solana_metrics_reporter_derive::ReportMetrics};
+pub use {
+    solana_metrics::datapoint_info,
+    solana_metrics_reporter_derive::{ReportMetrics, ReportMetricsInterval},
+};
 
 pub trait ReportMetrics {
     /// Reports metrics using datapoint_info!
     fn report(&self);
+}
+
+pub trait ReportMetricsInterval {
+    /// Reports metrics using datapoint_info! and resets fields
+    fn report(&mut self);
 }
