5daf38ecf8c98578168769f05e89df6d47ae3d94
Andrew Fitzgerald <apfitzge@gmail.com>
2022-10-05 14:37:01 -0500

    entrypoint cli arg

--- contentious-bench-tps/src/cli.rs
+++ contentious-bench-tps/src/cli.rs
@@ -1,6 +1,10 @@
-use clap::Parser;
+use {clap::Parser, std::net::SocketAddr};
 
 /// Arguments for the contentious-bench-tps program.
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
-pub struct Config {}
+pub struct Config {
+    /// Entrypoint to the cluster
+    #[clap(short, long, value_parser)]
+    pub entrypoint: SocketAddr,
+}
