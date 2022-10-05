7c1db4821e56f3e2e2be57b739e57dda9730b118
Andrew Fitzgerald <apfitzge@gmail.com>
2022-10-05 14:32:48 -0500

    Basic contentious-bench-tps cli setup

---
+++ contentious-bench-tps/src/cli.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+/// Arguments for the contentious-bench-tps program.
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct Config {}
--- contentious-bench-tps/src/main.rs
+++ contentious-bench-tps/src/main.rs
@@ -1,3 +1,11 @@
+use {clap::Parser, cli::Config, log::info};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    solana_logger::setup_with_default("solana=info");
+    solana_metrics::set_panic_hook("contentious-bench-tps", /*version:*/ None);
+
+    let config = Config::parse();
+    info!("config: {config:?}");
 }
