743bcee1e06fea6c7b7814d431c9670f1447383d
Andrew Fitzgerald <apfitzge@gmail.com>
2022-10-06 09:40:50 -0500

    add additional fee range cli args

--- contentious-bench-tps/src/cli.rs
+++ contentious-bench-tps/src/cli.rs
@@ -1,6 +1,6 @@
 use {
     clap::Parser,
-    std::{net::SocketAddr, time::Duration},
+    std::{net::SocketAddr, ops::RangeInclusive, time::Duration},
 };
 
 /// Arguments for the contentious-bench-tps program.
@@ -23,6 +23,10 @@ pub struct Config {
     #[clap(short, long, value_parser, default_value_t = 10)]
     pub num_contentious_transfer_accounts: usize,
 
+    /// Range of additional fees for contentious transfers in microlamports
+    #[clap(short, long, value_parser=parse_range_inclusive)]
+    pub contentious_transfer_fee_range: Option<RangeInclusive<u64>>,
+
     /// Number of threads to use for regular transfers
     #[clap(short, long, value_parser, default_value_t = 4)]
     pub num_regular_transfer_threads: usize,
@@ -30,6 +34,10 @@ pub struct Config {
     /// Number of non-contentious accounts for transfers
     #[clap(short, long, value_parser, default_value_t = 10000)]
     pub num_regular_transfer_accounts: usize,
+
+    /// Range of additional fees for regular transfers in microlamports
+    #[clap(short, long, value_parser=parse_range_inclusive)]
+    pub regular_transfer_fee_range: Option<RangeInclusive<u64>>,
 }
 
 /// Parse a duration in seconds from a string
@@ -37,4 +45,20 @@ fn parse_duration_from_secs(src: &str) -> Result<Duration, String> {
     src.parse::<u64>()
         .map(Duration::from_secs)
         .map_err(|err| err.to_string())
+}
+
+/// Parse a range of u64s from a string
+fn parse_range_inclusive(src: &str) -> Result<RangeInclusive<u64>, String> {
+    let mut parts = src.splitn(2, "-");
+    let start = parts
+        .next()
+        .unwrap()
+        .parse::<u64>()
+        .map_err(|err| err.to_string())?;
+    let end = parts
+        .next()
+        .unwrap()
+        .parse::<u64>()
+        .map_err(|err| err.to_string())?;
+    Ok(start..=end)
 }
