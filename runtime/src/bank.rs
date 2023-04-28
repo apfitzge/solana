568c7d481358f01df176db864e2d6c6ce6c0b471
Andrew Fitzgerald <apfitzge@gmail.com>
2023-04-28 21:48:03 +0000

    SQUASH ALL SCHEDULER

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4469,7 +4469,7 @@ impl Bank {
             .collect()
     }
 
-    fn check_transaction_age(
+    pub fn check_transaction_age(
         &self,
         tx: &SanitizedTransaction,
         max_age: usize,
@@ -4490,7 +4490,7 @@ impl Bank {
         }
     }
 
-    fn is_transaction_already_processed(
+    pub fn is_transaction_already_processed(
         &self,
         sanitized_tx: &SanitizedTransaction,
         status_cache: &BankStatusCache,
@@ -7820,6 +7820,20 @@ impl Bank {
     pub fn prevent_rent_paying_rent_recipients(&self) -> bool {
         self.feature_set
             .is_active(&feature_set::prevent_rent_paying_rent_recipients::id())
+    }
+
+    pub fn credits_auto_rewind(&self) -> bool {
+        self.feature_set
+            .is_active(&feature_set::credits_auto_rewind::id())
+    }
+
+    pub fn send_to_tpu_vote_port_enabled(&self) -> bool {
+        self.feature_set
+            .is_active(&feature_set::send_to_tpu_vote_port::id())
+    }
+
+    pub fn read_blockhash_queue(&self) -> LockResult<RwLockReadGuard<BlockhashQueue>> {
+        self.blockhash_queue.read()
     }
 
     pub fn read_cost_tracker(&self) -> LockResult<RwLockReadGuard<CostTracker>> {
