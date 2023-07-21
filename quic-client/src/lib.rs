eca0518e4488f746cbd59f23c9604e11a223ede6
Andrew Fitzgerald <apfitzge@gmail.com>
2023-07-21 19:09:26 +0000

    disable all fields - verify good perf on 2 runs

--- quic-client/src/lib.rs
+++ quic-client/src/lib.rs
@@ -216,10 +216,10 @@ impl ConnectionManager for QuicConnectionManager {
         } = self.connection_config.clone();
 
         QuicConfig {
-            client_certificate: cloned_client_certificate,
-            maybe_staked_nodes: cloned_maybe_staked_nodes,
-            maybe_client_pubkey: cloned_maybe_client_pubkey,
-            client_endpoint: cloned_client_endpoint,
+            // client_certificate: cloned_client_certificate,
+            // maybe_staked_nodes: cloned_maybe_staked_nodes,
+            // maybe_client_pubkey: cloned_maybe_client_pubkey,
+            // client_endpoint: cloned_client_endpoint,
             ..QuicConfig::new().unwrap()
         }
     }
