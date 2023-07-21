8ad5df1d9a855bbf8097f4ee3061b495cf221246
Andrew Fitzgerald <apfitzge@gmail.com>
2023-07-21 20:28:11 +0000

    test - client_certificate

--- quic-client/src/lib.rs
+++ quic-client/src/lib.rs
@@ -216,7 +216,7 @@ impl ConnectionManager for QuicConnectionManager {
         } = self.connection_config.clone();
 
         QuicConfig {
-            // client_certificate: cloned_client_certificate,
+            client_certificate: cloned_client_certificate,
             // maybe_staked_nodes: cloned_maybe_staked_nodes,
             // maybe_client_pubkey: cloned_maybe_client_pubkey,
             // client_endpoint: cloned_client_endpoint,
