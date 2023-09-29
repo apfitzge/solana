d878cc07aaf3ff6b4a8f7944a7d648d50e662795
Andrew Fitzgerald <apfitzge@gmail.com>
2023-09-29 00:06:48 +0000

    Bugfix: MultiIterator batch priority guard

--- core/src/banking_stage/unprocessed_transaction_storage.rs
+++ core/src/banking_stage/unprocessed_transaction_storage.rs
@@ -133,6 +133,7 @@ fn filter_processed_packets<'a, F>(
 pub struct ConsumeScannerPayload<'a> {
     pub reached_end_of_slot: bool,
     pub account_locks: ReadWriteAccountSet,
+    pub batch_priority_guard: ReadWriteAccountSet,
     pub sanitized_transactions: Vec<SanitizedTransaction>,
     pub slot_metrics_tracker: &'a mut LeaderSlotMetricsTracker,
     pub message_hash_to_transaction: &'a mut HashMap<Hash, DeserializedPacket>,
@@ -210,6 +211,7 @@ where
     let payload = ConsumeScannerPayload {
         reached_end_of_slot: false,
         account_locks: ReadWriteAccountSet::default(),
+        batch_priority_guard: ReadWriteAccountSet::default(),
         sanitized_transactions: Vec::with_capacity(UNPROCESSED_BUFFER_STEP_SIZE),
         slot_metrics_tracker,
         message_hash_to_transaction,
