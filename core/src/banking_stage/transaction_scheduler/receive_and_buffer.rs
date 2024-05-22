3501597d1b15d2b748811ad35557fc409fda882d
Andrew Fitzgerald <apfitzge@gmail.com>
2024-05-23 07:09:40 +0800

    TransactionViewReceiveAndBuffer

--- core/src/banking_stage/transaction_scheduler/receive_and_buffer.rs
+++ core/src/banking_stage/transaction_scheduler/receive_and_buffer.rs
@@ -3,25 +3,37 @@ use {
         scheduler_metrics::{SchedulerCountMetrics, SchedulerTimingMetrics},
         transaction_state_container::TransactionStateContainer,
     },
-    crate::banking_stage::{
-        decision_maker::BufferedPacketsDecision,
-        immutable_deserialized_packet::ImmutableDeserializedPacket,
-        packet_deserializer::PacketDeserializer,
-        transaction_scheduler::transaction_state::SanitizedTransactionTTL,
+    crate::{
+        banking_stage::{
+            decision_maker::BufferedPacketsDecision,
+            immutable_deserialized_packet::ImmutableDeserializedPacket,
+            packet_deserializer::PacketDeserializer,
+            transaction_scheduler::transaction_state::SanitizedTransactionTTL,
+        },
+        banking_trace::{BankingPacketBatch, BankingPacketReceiver},
+        transaction_view::TransactionView,
     },
+    arrayvec::ArrayVec,
     core::time::Duration,
-    crossbeam_channel::RecvTimeoutError,
+    crossbeam_channel::{RecvTimeoutError, TryRecvError},
     solana_cost_model::cost_model::CostModel,
     solana_fee::FeeBudgetLimits,
     solana_measure::measure_us,
+    solana_perf::packet::PACKETS_PER_BATCH,
     solana_program_runtime::compute_budget_processor::process_compute_budget_instructions,
     solana_runtime::{bank::Bank, bank_forks::BankForks},
     solana_sdk::{
-        clock::MAX_PROCESSING_AGE, saturating_add_assign, transaction::SanitizedTransaction,
+        clock::MAX_PROCESSING_AGE,
+        packet::{Packet, PacketFlags},
+        saturating_add_assign,
+        transaction::SanitizedTransaction,
     },
     solana_signed_message::{Message, SignedMessage},
     solana_svm::transaction_error_metrics::TransactionErrorMetrics,
-    std::sync::{Arc, RwLock},
+    std::{
+        sync::{Arc, RwLock},
+        time::Instant,
+    },
 };
 
 pub trait ReceiveAndBufferPackets<T: SignedMessage> {
@@ -226,6 +238,186 @@ impl SimpleReceiveAndBuffer {
                 );
             });
         }
+    }
+}
+
+pub struct TransactionViewReceiveAndBuffer {
+    receiver: BankingPacketReceiver,
+    bank_forks: Arc<RwLock<BankForks>>,
+}
+
+#[allow(dead_code)]
+impl TransactionViewReceiveAndBuffer {
+    pub fn new(receiver: BankingPacketReceiver, bank_forks: Arc<RwLock<BankForks>>) -> Self {
+        Self {
+            receiver,
+            bank_forks,
+        }
+    }
+
+    fn handle_message(
+        &self,
+        message: BankingPacketBatch,
+        _decision: &BufferedPacketsDecision,
+        _timing_metrics: &mut SchedulerTimingMetrics,
+        count_metrics: &mut SchedulerCountMetrics,
+        container: &mut TransactionStateContainer<TransactionView>,
+    ) {
+        let bank = self.bank_forks.read().unwrap().working_bank();
+        let last_slot_in_epoch = bank.epoch_schedule().get_last_slot_in_epoch(bank.epoch());
+        let transaction_account_lock_limit = bank.get_transaction_account_lock_limit();
+        let feature_set = &bank.feature_set;
+
+        let mut total_packet_count = 0;
+        let mut num_dropped_on_capacity: usize = 0;
+        let mut num_buffered: usize = 0;
+        let lock_results: [_; PACKETS_PER_BATCH] = core::array::from_fn(|_| Ok(()));
+        let mut error_counts = TransactionErrorMetrics::default();
+        for batch in &message.0 {
+            let valid_packet_references: ArrayVec<&Packet, PACKETS_PER_BATCH> =
+                ArrayVec::from_iter(batch.iter().filter(|p| !p.meta().discard()));
+            total_packet_count += valid_packet_references.len();
+
+            // Perform deserialization, sanitization, address resolution.
+            let mut packet_flags: ArrayVec<PacketFlags, PACKETS_PER_BATCH> = ArrayVec::new();
+            let transactions: ArrayVec<TransactionView, PACKETS_PER_BATCH> = ArrayVec::from_iter(
+                valid_packet_references
+                    .into_iter()
+                    .filter_map(|packet| {
+                        TransactionView::try_new(packet).map(|tv| (tv, packet.meta().flags))
+                    })
+                    .filter_map(|(mut tx, flags)| tx.sanitize().ok().map(|_| (tx, flags)))
+                    .filter_map(|(mut tx, flags)| {
+                        tx.resolve_addresses(&bank).ok().map(|_| (tx, flags))
+                    })
+                    .filter(|(tx, _flags)| {
+                        tx.validate_account_locks(transaction_account_lock_limit)
+                            .is_ok()
+                    })
+                    .filter(|(tx, _)| tx.verify_precompiles(feature_set).is_ok())
+                    .map(|(tx, flags)| {
+                        packet_flags.push(flags);
+                        tx
+                    }),
+            );
+
+            let check_results = bank.check_transactions(
+                &transactions,
+                &lock_results[..transactions.len()],
+                MAX_PROCESSING_AGE,
+                &mut error_counts,
+            );
+
+            for ((transaction, packet_flags), _) in transactions
+                .into_iter()
+                .zip(packet_flags.into_iter())
+                .zip(check_results)
+                .filter(|(_, check_result)| check_result.0.is_ok())
+            {
+                // Calculate fee budget limits.
+                let Ok(compute_budget_limits) =
+                    process_compute_budget_instructions(transaction.program_instructions_iter())
+                else {
+                    continue;
+                };
+                let fee_budget_limits = compute_budget_limits.into();
+
+                let (priority, cost) =
+                    calculate_priority_and_cost(&transaction, &fee_budget_limits, &bank);
+                let transaction_ttl = SanitizedTransactionTTL {
+                    transaction,
+                    max_age_slot: last_slot_in_epoch,
+                };
+
+                if container.insert_new_transaction(packet_flags, transaction_ttl, priority, cost) {
+                    saturating_add_assign!(num_dropped_on_capacity, 1);
+                }
+                saturating_add_assign!(num_buffered, 1);
+            }
+        }
+
+        count_metrics.update(|count_metrics| {
+            saturating_add_assign!(count_metrics.num_received, total_packet_count);
+            saturating_add_assign!(
+                count_metrics.num_dropped_on_capacity,
+                num_dropped_on_capacity
+            );
+            saturating_add_assign!(count_metrics.num_buffered, num_buffered);
+            // saturating_add_assign!(
+            //     count_metrics.num_dropped_on_sanitization,
+            //     num_dropped_on_sanitization
+            // );
+            // saturating_add_assign!(
+            //     count_metrics.num_dropped_on_validate_locks,
+            //     num_dropped_on_lock_validation
+            // );
+            // saturating_add_assign!(
+            //     count_metrics.num_dropped_on_receive_transaction_checks,
+            //     num_dropped_on_transaction_checks
+            // );
+        });
+    }
+}
+
+impl ReceiveAndBufferPackets<TransactionView> for TransactionViewReceiveAndBuffer {
+    /// Returns whether the packet receiver is still connected.
+    fn receive_and_buffer_packets(
+        &self,
+        decision: &BufferedPacketsDecision,
+        timing_metrics: &mut SchedulerTimingMetrics,
+        count_metrics: &mut SchedulerCountMetrics,
+        container: &mut TransactionStateContainer<TransactionView>,
+    ) -> bool {
+        const MAX_RECEIVE_AND_BUFFER_TIME: Duration = Duration::from_millis(100);
+        let now = Instant::now();
+
+        // Perform initial receive with timeout.
+        let (maybe_message, mut total_receive_time_us) =
+            measure_us!(self.receiver.recv_timeout(MAX_RECEIVE_AND_BUFFER_TIME));
+
+        let mut total_buffer_time_us = 0;
+        let mut connected = match maybe_message {
+            Ok(message) => {
+                let (_, buffer_time_us) = measure_us!(self.handle_message(
+                    message,
+                    decision,
+                    timing_metrics,
+                    count_metrics,
+                    container
+                ));
+                total_buffer_time_us += buffer_time_us;
+                true
+            }
+            Err(RecvTimeoutError::Timeout) => true,
+            Err(RecvTimeoutError::Disconnected) => false,
+        };
+
+        while connected && now.elapsed() < MAX_RECEIVE_AND_BUFFER_TIME {
+            let (maybe_message, receive_time_us) = measure_us!(self.receiver.try_recv());
+            total_receive_time_us += receive_time_us;
+            connected &= match maybe_message {
+                Ok(message) => {
+                    let (_, buffer_time_us) = measure_us!(self.handle_message(
+                        message,
+                        decision,
+                        timing_metrics,
+                        count_metrics,
+                        container
+                    ));
+                    total_buffer_time_us += buffer_time_us;
+                    true
+                }
+                Err(TryRecvError::Disconnected) => false,
+                Err(TryRecvError::Empty) => break, // no more messages
+            };
+        }
+
+        timing_metrics.update(|timing_metrics| {
+            saturating_add_assign!(timing_metrics.receive_time_us, total_receive_time_us);
+            saturating_add_assign!(timing_metrics.buffer_time_us, total_buffer_time_us);
+        });
+
+        connected
     }
 }
 
