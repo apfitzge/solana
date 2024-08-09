6001e44f204c1700febd3d5c2f899e9b17a67b8c
Andrew Fitzgerald <apfitzge@gmail.com>
2024-08-09 02:24:24 -0500

    iai_callgrind

--- transaction-view/benches/bytes_iai.rs
+++ transaction-view/benches/bytes_iai.rs
@@ -1,7 +1,18 @@
-use {agave_transaction_view::bytes::optimized_read_compressed_u16, iai::black_box};
+use {
+    agave_transaction_view::bytes::optimized_read_compressed_u16,
+    core::hint::black_box,
+    iai_callgrind::{library_benchmark, library_benchmark_group, main},
+};
 
-fn benchmark_optimized_read_compressed_u16_single_byte() {
-    let _result = optimized_read_compressed_u16(black_box(&[0b0100_0000]), &mut 0);
+#[library_benchmark]
+#[bench::single_byte(&[0x05])]
+fn bench_optimized_read_compressed_u16(bytes: &[u8]) {
+    black_box(optimized_read_compressed_u16(black_box(bytes), &mut 0)).unwrap();
 }
 
-iai::main!(benchmark_optimized_read_compressed_u16_single_byte);
+library_benchmark_group!(
+    name = compressed_u16;
+    benchmarks = bench_optimized_read_compressed_u16
+);
+
+main!(library_benchmark_groups = compressed_u16);
