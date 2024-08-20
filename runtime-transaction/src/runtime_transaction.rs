5a19385f5724764e7034b0f607901a57a99b799d
Andrew Fitzgerald <apfitzge@gmail.com>
2024-08-20 11:30:14 -0500

    RuntimeTransaction impl SVMTransaction

--- runtime-transaction/src/runtime_transaction.rs
+++ runtime-transaction/src/runtime_transaction.rs
@@ -22,7 +22,9 @@ use {
         simple_vote_transaction_checker::is_simple_vote_transaction,
         transaction::{Result, SanitizedTransaction, SanitizedVersionedTransaction},
     },
-    solana_svm_transaction::instruction::SVMInstruction,
+    solana_svm_transaction::{
+        instruction::SVMInstruction, svm_message::SVMMessage, svm_transaction::SVMTransaction,
+    },
     std::collections::HashSet,
 };
 
@@ -131,6 +133,74 @@ impl RuntimeTransaction<SanitizedTransaction> {
 
     fn load_dynamic_metadata(&mut self) -> Result<()> {
         Ok(())
+    }
+}
+
+impl<T: SVMMessage> SVMMessage for RuntimeTransaction<T> {
+    fn num_total_signatures(&self) -> u64 {
+        self.transaction.num_total_signatures()
+    }
+
+    fn num_write_locks(&self) -> u64 {
+        self.transaction.num_write_locks()
+    }
+
+    fn recent_blockhash(&self) -> &Hash {
+        self.transaction.recent_blockhash()
+    }
+
+    fn num_instructions(&self) -> usize {
+        self.transaction.num_instructions()
+    }
+
+    fn instructions_iter(&self) -> impl Iterator<Item = SVMInstruction> {
+        self.transaction.instructions_iter()
+    }
+
+    fn program_instructions_iter(&self) -> impl Iterator<Item = (&Pubkey, SVMInstruction)> {
+        self.transaction.program_instructions_iter()
+    }
+
+    fn account_keys(&self) -> solana_sdk::message::AccountKeys {
+        self.transaction.account_keys()
+    }
+
+    fn fee_payer(&self) -> &Pubkey {
+        self.transaction.fee_payer()
+    }
+
+    fn is_writable(&self, index: usize) -> bool {
+        self.transaction.is_writable(index)
+    }
+
+    fn is_signer(&self, index: usize) -> bool {
+        self.transaction.is_signer(index)
+    }
+
+    fn is_invoked(&self, key_index: usize) -> bool {
+        self.transaction.is_invoked(key_index)
+    }
+
+    fn num_lookup_tables(&self) -> usize {
+        self.transaction.num_lookup_tables()
+    }
+
+    fn message_address_table_lookups(
+        &self,
+    ) -> impl Iterator<
+        Item = solana_svm_transaction::message_address_table_lookup::SVMMessageAddressTableLookup,
+    > {
+        self.transaction.message_address_table_lookups()
+    }
+}
+
+impl<T: SVMTransaction> SVMTransaction for RuntimeTransaction<T> {
+    fn signature(&self) -> &solana_sdk::signature::Signature {
+        self.transaction.signature()
+    }
+
+    fn signatures(&self) -> &[solana_sdk::signature::Signature] {
+        self.transaction.signatures()
     }
 }
 
