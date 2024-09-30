390719238a6fbbd14b9c6fd3a4a0d5c6d60804f7
Andrew Fitzgerald <apfitzge@gmail.com>
2024-09-30 10:41:15 -0500

    unneccessary trait bound

--- perf/src/mutable_packet_batch.rs
+++ perf/src/mutable_packet_batch.rs
@@ -3,7 +3,7 @@ use {crate::packet::PacketBatch, core::borrow::Borrow, std::sync::Arc};
 /// Helper trait to make it simple to work with:
 /// - `&mut [PacketBatch]` or
 /// - `&mut [Arc<PacketBatch>]`
-pub trait MutablePacketBatch: Borrow<PacketBatch> + Send + Sync {
+pub trait MutablePacketBatch: Borrow<PacketBatch> + Send {
     fn as_mut(&mut self) -> &mut PacketBatch;
 }
 
