fd52b88cb14b47ce2fe2d68191b0d94493a36dd0
Andrew Fitzgerald <apfitzge@gmail.com>
2024-10-17 17:05:18 -0500

    fix sbf tests

--- programs/sbf/tests/programs.rs
+++ programs/sbf/tests/programs.rs
@@ -3886,7 +3886,6 @@ fn test_program_fees() {
     );
     let expected_normal_fee = solana_fee::calculate_fee(
         &sanitized_message,
-        congestion_multiplier == 0,
         fee_structure.lamports_per_signature,
         fee_budget_limits.prioritization_fee,
         true,
@@ -3918,7 +3917,6 @@ fn test_program_fees() {
     );
     let expected_prioritized_fee = solana_fee::calculate_fee(
         &sanitized_message,
-        congestion_multiplier == 0,
         fee_structure.lamports_per_signature,
         fee_budget_limits.prioritization_fee,
         true,
