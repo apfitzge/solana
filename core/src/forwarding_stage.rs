9ddba48e5145a733e0a4b2da9280c78e73f10d9a
Andrew Fitzgerald <apfitzge@gmail.com>
2024-10-21 11:03:52 -0500

    test_tpu_vote_forwarding

--- core/src/forwarding_stage.rs
+++ core/src/forwarding_stage.rs
@@ -124,3 +124,89 @@ impl<T: ForwardAddressGetter> ForwardingStage<T> {
         });
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        solana_perf::packet::PacketBatch,
+        solana_sdk::packet::{Packet, PacketFlags},
+        std::time::Duration,
+    };
+
+    struct DummyForwardAddressGetter(Option<SocketAddr>);
+    impl ForwardAddressGetter for DummyForwardAddressGetter {
+        fn get_forwarding_address(
+            &self,
+            _tpu_vote: bool,
+            _protocol: Protocol,
+        ) -> Option<SocketAddr> {
+            self.0
+        }
+    }
+
+    #[test]
+    fn test_tpu_vote_forwarding() {
+        let forward_socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+        let forward_addr = forward_socket.local_addr().unwrap();
+
+        let (_sender, receiver) = crossbeam_channel::unbounded();
+        let dummy_forward_address_getter = DummyForwardAddressGetter(Some(forward_addr));
+        let connection_cache = ConnectionCache::new("connection_cache_test");
+
+        let forwarding_stage = ForwardingStage::new(
+            receiver,
+            dummy_forward_address_getter,
+            Arc::new(connection_cache),
+        );
+
+        const NUM_BYTES: usize = 8;
+        let mut packet = Packet::default();
+        packet.populate_packet(None, &[0u8; NUM_BYTES]).unwrap(); // we don't need actual content here
+        packet.meta_mut().set_simple_vote(true);
+        packet.meta_mut().set_from_staked_node(true);
+
+        forward_socket
+            .set_read_timeout(Some(Duration::from_millis(10)))
+            .unwrap();
+        let recv_buffer = &mut [0; 1024];
+
+        // Data Budget is 0, so no packets should be sent
+        forwarding_stage.forward_batch(
+            BankingPacketBatch::new((vec![PacketBatch::new(vec![packet.clone()])], None)),
+            true,
+            forward_addr,
+        );
+        assert!(forward_socket.recv_from(recv_buffer).is_err());
+
+        // Packet is valid, so it should be sent
+        forwarding_stage.update_data_budget();
+        forwarding_stage.forward_batch(
+            BankingPacketBatch::new((vec![PacketBatch::new(vec![packet.clone()])], None)),
+            true,
+            forward_addr,
+        );
+        assert_eq!(forward_socket.recv_from(recv_buffer).unwrap().0, NUM_BYTES);
+
+        // Packet is not from staked node, so it should not be sent
+        forwarding_stage.update_data_budget();
+        packet.meta_mut().set_from_staked_node(false);
+        forwarding_stage.forward_batch(
+            BankingPacketBatch::new((vec![PacketBatch::new(vec![packet.clone()])], None)),
+            true,
+            forward_addr,
+        );
+        assert!(forward_socket.recv_from(recv_buffer).is_err());
+
+        // Packet is already forwarded, so it should not be sent
+        forwarding_stage.update_data_budget();
+        packet.meta_mut().set_from_staked_node(true);
+        packet.meta_mut().flags |= PacketFlags::FORWARDED;
+        forwarding_stage.forward_batch(
+            BankingPacketBatch::new((vec![PacketBatch::new(vec![packet.clone()])], None)),
+            true,
+            forward_addr,
+        );
+        assert!(forward_socket.recv_from(recv_buffer).is_err());
+    }
+}
