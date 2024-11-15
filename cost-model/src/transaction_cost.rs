6b363d0412ba1c1b20b2019bb6d578c014d40b5c
Andrew Fitzgerald <apfitzge@gmail.com>
2024-11-15 14:58:55 -0600

    simply TransactionCost

--- cost-model/src/transaction_cost.rs
+++ cost-model/src/transaction_cost.rs
@@ -1,7 +1,10 @@
 use {
     crate::block_cost_limits,
-    solana_runtime_transaction::transaction_with_meta::TransactionWithMeta,
+    solana_runtime_transaction::{
+        transaction_meta::StaticMeta, transaction_with_meta::TransactionWithMeta,
+    },
     solana_sdk::pubkey::Pubkey,
+    solana_svm_transaction::svm_message::SVMMessage,
 };
 
 /// TransactionCost is used to represent resources required to process
@@ -15,12 +18,12 @@ use {
 const SIMPLE_VOTE_USAGE_COST: u64 = 3428;
 
 #[derive(Debug)]
-pub enum TransactionCost<'a, Tx: TransactionWithMeta> {
+pub enum TransactionCost<'a, Tx> {
     SimpleVote { transaction: &'a Tx },
     Transaction(UsageCostDetails<'a, Tx>),
 }
 
-impl<'a, Tx: TransactionWithMeta> TransactionCost<'a, Tx> {
+impl<'a, Tx> TransactionCost<'a, Tx> {
     pub fn sum(&self) -> u64 {
         #![allow(clippy::assertions_on_constants)]
         match self {
@@ -88,7 +91,9 @@ impl<'a, Tx: TransactionWithMeta> TransactionCost<'a, Tx> {
             Self::Transaction(usage_cost) => usage_cost.write_lock_cost,
         }
     }
+}
 
+impl<Tx: SVMMessage> TransactionCost<'_, Tx> {
     pub fn writable_accounts(&self) -> impl Iterator<Item = &Pubkey> {
         let transaction = match self {
             Self::SimpleVote { transaction } => transaction,
@@ -100,7 +105,9 @@ impl<'a, Tx: TransactionWithMeta> TransactionCost<'a, Tx> {
             .enumerate()
             .filter_map(|(index, key)| transaction.is_writable(index).then_some(key))
     }
+}
 
+impl<Tx: StaticMeta> TransactionCost<'_, Tx> {
     pub fn num_transaction_signatures(&self) -> u64 {
         match self {
             Self::SimpleVote { .. } => 1,
@@ -134,7 +141,7 @@ impl<'a, Tx: TransactionWithMeta> TransactionCost<'a, Tx> {
 
 // costs are stored in number of 'compute unit's
 #[derive(Debug)]
-pub struct UsageCostDetails<'a, Tx: TransactionWithMeta> {
+pub struct UsageCostDetails<'a, Tx> {
     pub transaction: &'a Tx,
     pub signature_cost: u64,
     pub write_lock_cost: u64,
@@ -144,7 +151,7 @@ pub struct UsageCostDetails<'a, Tx: TransactionWithMeta> {
     pub allocated_accounts_data_size: u64,
 }
 
-impl<'a, Tx: TransactionWithMeta> UsageCostDetails<'a, Tx> {
+impl<'a, Tx> UsageCostDetails<'a, Tx> {
     pub fn sum(&self) -> u64 {
         self.signature_cost
             .saturating_add(self.write_lock_cost)
