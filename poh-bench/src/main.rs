dbf009d516d5678ee5d7cb6a9b15ab3b611d621f
Andrew Fitzgerald <apfitzge@gmail.com>
2024-11-27 06:30:35 -0600

    fix bench

--- poh-bench/src/main.rs
+++ poh-bench/src/main.rs
@@ -100,7 +100,7 @@ fn main() {
                 for _ in 0..iterations {
                     assert!(ticks[..num_entries]
                         .verify_cpu_x86_simd(&start_hash, 8, &thread_pool)
-                        .finish_verify(&thread_pool));
+                        .finish_verify());
                 }
                 time.stop();
                 println!(
@@ -115,7 +115,7 @@ fn main() {
                 for _ in 0..iterations {
                     assert!(ticks[..num_entries]
                         .verify_cpu_x86_simd(&start_hash, 16, &thread_pool)
-                        .finish_verify(&thread_pool));
+                        .finish_verify());
                 }
                 time.stop();
                 println!(
