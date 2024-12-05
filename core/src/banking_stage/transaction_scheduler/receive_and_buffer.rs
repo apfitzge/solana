8e3eb243af7e2f4f95eefa92021eb6cb4568078b
Andrew Fitzgerald <apfitzge@gmail.com>
2024-12-05 11:06:30 -0600

    fix conflict w/ sanitize_and_convert_to_compute_budget_limits

--- core/src/banking_stage/transaction_scheduler/receive_and_buffer.rs
+++ core/src/banking_stage/transaction_scheduler/receive_and_buffer.rs
@@ -515,7 +515,7 @@ impl TransactionViewReceiveAndBuffer {
 
         let Ok(compute_budget_limits) = view
             .compute_budget_instruction_details()
-            .sanitize_and_convert_to_compute_budget_limits()
+            .sanitize_and_convert_to_compute_budget_limits(&working_bank.feature_set)
         else {
             return Err(());
         };
