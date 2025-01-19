4a503d7ea5734cb72d5e655eea789fc09bda26e9
Andrew Fitzgerald <apfitzge@gmail.com>
2025-01-19 17:47:16 -0600

    receive_completed before schedule call

--- core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
+++ core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
@@ -119,8 +119,8 @@ impl SchedulerController {
             self.timing_metrics
                 .maybe_report_and_reset_slot(new_leader_slot);
 
-            self.process_transactions(&decision)?;
             self.receive_completed()?;
+            self.process_transactions(&decision)?;
             if !self.receive_and_buffer_packets(&decision) {
                 break;
             }
