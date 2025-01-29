91ca23c673f9335976270d8e1bf1bfca35ab29cb
Andrew Fitzgerald <apfitzge@gmail.com>
2025-01-29 11:07:25 -0600

    Scheduler trait

--- core/src/banking_stage/transaction_scheduler/mod.rs
+++ core/src/banking_stage/transaction_scheduler/mod.rs
@@ -2,6 +2,7 @@ mod batch_id_generator;
 mod in_flight_tracker;
 pub(crate) mod prio_graph_scheduler;
 pub(crate) mod receive_and_buffer;
+pub(crate) mod scheduler;
 pub(crate) mod scheduler_controller;
 pub(crate) mod scheduler_error;
 mod scheduler_metrics;
