17e2f1a781e77e52225f0bb0b483acb41a988094
Andrew Fitzgerald <apfitzge@gmail.com>
2025-02-06 08:18:25 -0600

    remove copy_return_values

--- perf/src/sigverify.rs
+++ perf/src/sigverify.rs
@@ -482,21 +482,6 @@ pub fn ed25519_verify_disabled(batches: &mut [PacketBatch]) {
     });
 }
 
-pub fn copy_return_values<I, T>(sig_lens: I, out: &RecycledVec<u8>, rvs: &mut [Vec<u8>])
-where
-    I: IntoIterator<Item = T>,
-    T: IntoIterator<Item = u32>,
-{
-    debug_assert!(rvs.iter().flatten().all(|&rv| rv == 0u8));
-    let mut offset = 0usize;
-    let rvs = rvs.iter_mut().flatten();
-    for (k, rv) in sig_lens.into_iter().flatten().zip(rvs) {
-        let out = out[offset..].iter().take(k as usize).all(|&x| x == 1u8);
-        *rv = u8::from(k != 0u32 && out);
-        offset = offset.saturating_add(k as usize);
-    }
-}
-
 pub fn mark_disabled(batches: &mut [PacketBatch], r: &[Vec<u8>]) {
     for (batch, v) in batches.iter_mut().zip(r) {
         for (pkt, f) in batch.iter_mut().zip(v) {
@@ -533,7 +518,6 @@ mod tests {
         solana_signature::Signature,
         solana_signer::Signer,
         solana_transaction::Transaction,
-        std::iter::repeat_with,
     };
 
     const SIG_OFFSET: usize = 1;
@@ -542,46 +526,6 @@ mod tests {
         assert!(a.len() >= b.len());
         let end = a.len() - b.len() + 1;
         (0..end).find(|&i| a[i..i + b.len()] == b[..])
-    }
-
-    #[test]
-    fn test_copy_return_values() {
-        let mut rng = rand::thread_rng();
-        let sig_lens: Vec<Vec<u32>> = {
-            let size = rng.gen_range(0..64);
-            repeat_with(|| {
-                let size = rng.gen_range(0..16);
-                repeat_with(|| rng.gen_range(0..5)).take(size).collect()
-            })
-            .take(size)
-            .collect()
-        };
-        let out: Vec<Vec<Vec<bool>>> = sig_lens
-            .iter()
-            .map(|sig_lens| {
-                sig_lens
-                    .iter()
-                    .map(|&size| repeat_with(|| rng.gen()).take(size as usize).collect())
-                    .collect()
-            })
-            .collect();
-        let expected: Vec<Vec<u8>> = out
-            .iter()
-            .map(|out| {
-                out.iter()
-                    .map(|out| u8::from(!out.is_empty() && out.iter().all(|&k| k)))
-                    .collect()
-            })
-            .collect();
-        let out = RecycledVec::<u8>::from_vec(
-            out.into_iter().flatten().flatten().map(u8::from).collect(),
-        );
-        let mut rvs: Vec<Vec<u8>> = sig_lens
-            .iter()
-            .map(|sig_lens| vec![0u8; sig_lens.len()])
-            .collect();
-        copy_return_values(sig_lens, &out, &mut rvs);
-        assert_eq!(rvs, expected);
     }
 
     #[test]
